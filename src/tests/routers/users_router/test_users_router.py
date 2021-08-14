b5635268fc2761adb2e24886ee81f37e9dca4a50
Michael Savin <jtprogru@gmail.com>
2021-08-15 00:14:19 +0300

    Сделал работающий mock для запроса на создание пользователя
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- src/tests/routers/users_router/test_users_router.py
+++ src/tests/routers/users_router/test_users_router.py
@@ -1,20 +1,19 @@
 import pytest
+import requests_mock
 
 
 def test_get_users_list(client):
     """Получение списка всех пользователей"""
     response = client.get("/api/v1/users/")
-
     assert isinstance(response.json(), list)
     assert response.json() == []
     assert response.status_code == 200
 
 
-@pytest.mark.xfail
-def test_create_user(client, default_user_data):
-    response = client.post("/api/v1/users/", data=default_user_data)
+def test_create_user(mocked_user_create, default_user_data):
+    response = mocked_user_create
 
-    # assert response.status_code == 201
+    assert response.status_code == 200
     assert response.json() == {
         "username": "kamaz",
         "email": "kamaz@email.com",
