4713b77be4577cbb8023fd3de0f720283356b4b6
Michael Savin <jtprogru@gmail.com>
2021-08-17 00:36:21 +0300

    fix-linters-errors #3 - Fix docstrings

--- src/app/services/db.py
+++ src/app/services/db.py
@@ -1,13 +1,41 @@
+"""Сервисный модуль для базы данных."""
 from app.core import Base, SessionLocal, engine
 
 
-async def create_database():
+class DatabaseException(Exception):
+    """Класс исключений."""
+
+    def with_traceback(self, tb) -> str:
+        """Возврат трейсбека.
+
+        Args:
+            tb: трейсбек
+
+        Returns:
+            str: форматированная строка
+        """
+        return f'{self.__class__.__name__}: {self.__traceback__}'
+
+
+async def create_database() -> Base:
+    """Создание базы данных.
+
+    Returns:
+        Base.metadata.create_all(bind=engine): создание базы
+    """
     return Base.metadata.create_all(bind=engine)
 
 
-async def get_db():
+async def get_db() -> SessionLocal:
+    """Получение экземпляра базы данных.
+
+    Yields:
+        db: SessionLocal экземпляр
+    """
     db = SessionLocal()
     try:
         yield db
+    except DatabaseException as err:
+        err.with_traceback(tb=err)
     finally:
         db.close()
