c266de89a33cf4f1a28004e158c5df8443d4a7bc
Michael Savin <jtprogru@gmail.com>
2021-08-17 13:44:49 +0300

    fix-linters-errors #3 - Дописал docstrings для модуля services
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- src/app/services/users.py
+++ src/app/services/users.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 
 from app import schemas
 from app.models.users import User
+from app.services.security import get_password_hash
 
 
 async def get_user(db: Session, user_id: int) -> User:
@@ -15,24 +16,52 @@ async def get_user(db: Session, user_id: int) -> User:
         user_id: - ID пользователя
 
     Returns:
-        models.User: - экземпляр записи из БД
+        User: - экземпляр записи из БД
     """
     return db.query(User).filter(User.id == user_id).first()
 
 
-async def get_user_by_email(db: Session, email: str):
+async def get_user_by_email(db: Session, email: str) -> User:
+    """Получение пользователя по email.
+
+    Args:
+        db: - Сессия к БД
+        email: - email пользователя
+
+    Returns:
+        User: - экземпляр записи из БД
+    """
     return db.query(User).filter(User.email == email).first()
 
 
-async def get_users_list(db: Session, skip: int = 0, limit: int = 100):
+async def get_users_list(db: Session, skip: int = 0, limit: int = 100) -> list:
+    """Получение списка пользователей.
+
+    Args:
+        db: - Сессия к БД
+        skip: - сколько пропустить
+        limit: - сколько выдать
+
+    Returns:
+        list: - список записей из БД
+    """
     users_list = db.query(User).offset(skip).limit(limit).all()
     if users_list:
         return users_list
     return []
 
 
-async def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + 'thisisnotsecure'
+async def create_user(db: Session, user: schemas.UserCreate) -> User:
+    """Получение пользователя по email.
+
+    Args:
+        db: - Сессия к БД
+        user: - схема с пользовательскими данными
+
+    Returns:
+        User: - экземпляр записи из БД
+    """
+    fake_hashed_password = get_password_hash(user.password)
     db_user = User(
         username=user.username,
         registered_datetime=datetime.utcnow(),
@@ -46,8 +75,17 @@ async def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 
-async def delete_user_by_id(db: Session, user_id: int):
+async def delete_user_by_id(db: Session, user_id: int) -> dict:
+    """Удаление пользователя по ID.
+
+    Args:
+        db: - Сессия к БД
+        user_id: - ID пользователя
+
+    Returns:
+        dict: - сообщение об удалении пользователя
+    """
     user_db = db.query(User).filter(User.id == user_id).first()
     db.delete(user_db)
     db.flush()
-    return
+    return {'message': f'User with {user_id} was deleted'}
